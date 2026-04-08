49d598c7d076ab8bbe700be07d08c9db7f627276
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-07 22:31:44 -0300

    feat: add joda date time

---
+++ src/main/java/com/myprojecticaro/poc_java_joda_time/controller/DateController.java
@@ -0,0 +1,27 @@
+package com.myprojecticaro.poc_java_joda_time.controller;
+
+import com.myprojecticaro.poc_java_joda_time.service.DateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/date")
+public class DateController {
+
+    private final DateService service;
+
+    public DateController(DateService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/java-time")
+    public String javaTime() {
+        return service.getJavaTime();
+    }
+
+    @GetMapping("/joda")
+    public String jodaTime() {
+        return service.getJodaTime();
+    }
+}
