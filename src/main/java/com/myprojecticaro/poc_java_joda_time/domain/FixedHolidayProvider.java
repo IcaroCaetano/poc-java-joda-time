dc2a4d482f97627675e85a45ccd9ee9360ace6ca
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-07 22:45:10 -0300

    feat: add joda date time

---
+++ src/main/java/com/myprojecticaro/poc_java_joda_time/domain/FixedHolidayProvider.java
@@ -0,0 +1,20 @@
+package com.myprojecticaro.poc_java_joda_time.domain;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+public class FixedHolidayProvider implements HolidayProvider {
+
+    private final Set<LocalDate> holidays;
+
+    public FixedHolidayProvider(Set<LocalDate> holidays) {
+
+        this.holidays = holidays;
+    }
+
+    @Override
+    public boolean isHoliday(LocalDate date) {
+
+        return holidays.contains(date);
+    }
+}
