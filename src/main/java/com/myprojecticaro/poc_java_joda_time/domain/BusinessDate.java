105fe641211d63b7001ff878a33739b4d8eae94d
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-07 22:42:03 -0300

    feat: add joda date time

--- src/main/java/com/myprojecticaro/poc_java_joda_time/domain/BusinessDate.java
+++ src/main/java/com/myprojecticaro/poc_java_joda_time/domain/BusinessDate.java
@@ -1,20 +1,33 @@
 package com.myprojecticaro.poc_java_joda_time.domain;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public record BusinessDate(LocalDate date) {
 
     public boolean isWeekend() {
-
-        return date.getDayOfWeek().getValue() >= 6;
+        return date.getDayOfWeek() == DayOfWeek.SATURDAY ||
+                date.getDayOfWeek() == DayOfWeek.SUNDAY;
     }
+
+
+    public boolean isBusinessDay() {
+        return !isWeekend();
+    }
+
 
     public BusinessDate nextBusinessDay() {
         LocalDate next = date.plusDays(1);
 
-        while (next.getDayOfWeek().getValue() >= 6) {
+        while (isWeekend(next)) {
             next = next.plusDays(1);
         }
+
         return new BusinessDate(next);
+    }
+
+    private boolean isWeekend(LocalDate d) {
+        return d.getDayOfWeek() == DayOfWeek.SATURDAY ||
+                d.getDayOfWeek() == DayOfWeek.SUNDAY;
     }
 }
