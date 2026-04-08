191156686b7e30328d70a01e36e25fd8aa683634
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-08 12:32:09 -0300

    test: business date

---
+++ src/test/java/com/myprojecticaro/poc_java_joda_time/domain/model/BusinessDateTest.java
@@ -0,0 +1,37 @@
+package com.myprojecticaro.poc_java_joda_time.domain.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessDateTest {
+
+    @Test
+    void shouldReturnTrue_whenSaturday() {
+        BusinessDate date = new BusinessDate(LocalDate.of(2026, 4, 11));
+
+        assertTrue(date.isWeekend());
+
+        //Saida - Sabasdo
+    }
+
+    @Test
+    void shouldReturnTrue_whenSunday() {
+        BusinessDate date = new BusinessDate(LocalDate.of(2026, 4, 12));
+
+        assertTrue(date.isWeekend());
+
+        //Saida - domingo
+    }
+
+    @Test
+    void shouldReturnFalse_whenWeekday() {
+        BusinessDate date = new BusinessDate(LocalDate.of(2026, 4, 8));
+
+        assertFalse(date.isWeekend());
+
+        //Saida - quarta
+    }
+}
