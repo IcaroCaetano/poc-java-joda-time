d3d4b765c849473e6c5c094beff44118716ffb3e
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-07 22:22:54 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/poc_java_joda_time/PocJavaJodaTimeApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_java_joda_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJavaJodaTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJavaJodaTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_java_joda_time/PocJavaJodaTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_java_joda_time;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJavaJodaTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
