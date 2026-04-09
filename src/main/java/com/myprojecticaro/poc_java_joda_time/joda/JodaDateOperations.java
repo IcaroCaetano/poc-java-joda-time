900ff8eb844a37c1e618540919443457ab133e48
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-09 09:50:04 -0300

    feat: update joda time

--- src/main/java/com/myprojecticaro/poc_java_joda_time/joda/JodaDateOperations.java
+++ src/main/java/com/myprojecticaro/poc_java_joda_time/joda/JodaDateOperations.java
@@ -9,13 +9,36 @@ public class JodaDateOperations {
         return new DateTime().plusDays(days);
     }
 
-    public static DateTime minusHours(int hours) {
-
-        return new DateTime().minusHours(hours);
+    public static DateTime addHours(int hours) {
+        return new DateTime().plusHours(hours);
     }
 
     public static DateTime addMinutes(int minutes) {
 
         return new DateTime().plusMinutes(minutes);
+    }
+
+    public static DateTime addSeconds(int seconds) {
+        return new DateTime().plusSeconds(seconds);
+    }
+
+    public static DateTime minusDays(int days) {
+
+        return new DateTime().minusDays(days);
+    }
+
+    public static DateTime minusHours(int hours) {
+
+        return new DateTime().minusHours(hours);
+    }
+
+    public static DateTime minusMinutes(int minutes) {
+
+        return new DateTime().minusMinutes(minutes);
+    }
+
+    public static DateTime minusSeconds(int seconds) {
+
+        return new DateTime().minusSeconds(seconds);
     }
 }
