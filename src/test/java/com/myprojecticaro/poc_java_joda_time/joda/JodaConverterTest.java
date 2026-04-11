4684a2a0de440032824d41dfbaae0433cce6e5b1
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-11 17:53:40 -0300

    feat: add tests

--- src/test/java/com/myprojecticaro/poc_java_joda_time/joda/JodaConverterTest.java
+++ src/test/java/com/myprojecticaro/poc_java_joda_time/joda/JodaConverterTest.java
@@ -2,10 +2,12 @@ package com.myprojecticaro.poc_java_joda_time.joda;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -60,11 +62,36 @@ class JodaConverterTest {
 
         LocalDateTime javaTime = JodaConverter.toJavaTime(joda);
 
+        System.out.println(javaTime);
+
+        // Saida: javaTime: 2026-04-08T07:00
+
         ZoneId systemZone = ZoneId.systemDefault();
 
+        // Saida: systemZone: America/Fortaleza
+
+        System.out.println(systemZone);
+
+        Instant instantJodaTime = joda.toInstant();
+
+        System.out.println(instantJodaTime);
+
+        // Saida: instantJodaTime: 2026-04-08T10:00:00.000Z
+
+        Date dateJavaUtil = instantJodaTime.toDate();
+
+        System.out.println(dateJavaUtil);
+
+        // Saida: dateJavaUtil: Wed Apr 08 07:00:00 BRT 2026
+
+        java.time.Instant javaTimeInstant = dateJavaUtil.toInstant();
+
+        System.out.println(javaTimeInstant);
+
+        // Saida: javaTimeInstant: 2026-04-08T10:00:00Z
+
         LocalDateTime expected = LocalDateTime.ofInstant(
-                joda.toInstant().toDate().toInstant(),
-                systemZone
+                javaTimeInstant, systemZone
         );
 
         assertEquals(expected, javaTime);
